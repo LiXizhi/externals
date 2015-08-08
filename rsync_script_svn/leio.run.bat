fdab2b8db7ab2244dc1547ad03f49432e7a6a776
leio <zhangleio@outlook.com>
2015-08-08 14:46:33 +0800

    added cwRsync

---
+++ rsync_script_svn/leio.run.bat
@@ -0,0 +1,2 @@
+cd K:\work\ParaEngnie-externals-Git\rsync_script_svn\cwRsync_5.4.1_x86_Free
+rsync -vzrtopg --delete --progress update_haqi@115.29.230.154::script_svn ./../test_svn --password-file=../rsync_db.leio.ps
